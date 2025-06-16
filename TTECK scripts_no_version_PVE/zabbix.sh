4804ef73ee14471467e61923b39613522bad4dc5
Pavel Spirin <p.spirin@cersanit.ru>
2025-06-16 12:05:35 +0400

    Auto commit from batch script

--- TTECK scripts_no_version_PVE/zabbix.sh
+++ TTECK scripts_no_version_PVE/zabbix.sh
@@ -1,82 +1,82 @@
 #!/usr/bin/env bash
 
-# Подключение функции сборки от TTECK
-source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+# ---------- Настройки ----------
+CT_ID=$(pvesh get /cluster/nextid)
+HOSTNAME="zabbix"
+DISK_SIZE="6G"
+CPU_COUNT="2"
+RAM_SIZE="4096"
+BRIDGE="vmbr0"
+TEMPLATE="debian-12-standard_*.tar.zst"
+STORAGE="local-lvm"
+ZBX_PORT=80
 
-# Авторское право и лицензия
-# Copyright (c) 2021-2024 tteck
-# Author: tteck (tteckster)
-# License: MIT
+# ---------- Проверки ----------
+echo "? Проверка наличия шаблона Debian 12..."
+if ! pveam list | grep -q "$TEMPLATE"; then
+    echo "?? Скачивание шаблона Debian 12..."
+    pveam update
+    pveam download local debian-12-standard_2023-*.tar.zst
+fi
 
-function header_info {
-clear
-cat <<"EOF"
- _____         __    __    _
-/__  /  ____ _/ /_  / /_  (_)  __
-  / /  / __ `/ __ \/ __ \/ / |/_/
- / /__/ /_/ / /_/ / /_/ / />  <
-/____/\__,_/_.___/_.___/_/_/|_|
+echo "?? Создание LXC контейнера ($CT_ID)..."
+pct create $CT_ID $(pveam list local | grep debian-12 | awk '{print $1}') \
+    -hostname $HOSTNAME \
+    -cores $CPU_COUNT \
+    -memory $RAM_SIZE \
+    -rootfs $STORAGE:$DISK_SIZE \
+    -net0 name=eth0,bridge=$BRIDGE,ip=dhcp \
+    -features nesting=1 \
+    -unprivileged 1 \
+    -ostype debian \
+    -startup order=2
 
-EOF
-}
-header_info
-echo -e "Loading..."
+echo "?? Запуск контейнера..."
+pct start $CT_ID
+sleep 5
 
-APP="Zabbix"
-var_disk="6"
-var_cpu="2"
-var_ram="4096"
-var_os="debian"
-var_version="12"
+echo "?? Получение IP-адреса контейнера..."
+CT_IP=""
+for i in {1..10}; do
+  CT_IP=$(pct exec $CT_ID -- hostname -I | awk '{print $1}')
+  [[ -n "$CT_IP" ]] && break
+  sleep 2
+done
 
-# Загружаем переменные и функции из build.func
-variables
-color
-catch_errors
+if [[ -z "$CT_IP" ]]; then
+    echo "? Не удалось получить IP контейнера"
+    exit 1
+fi
 
-function default_settings() {
-  CT_TYPE="1"
-  PW=""
-  CT_ID=$NEXTID
-  HN=$NSAPP
-  DISK_SIZE="$var_disk"
-  CORE_COUNT="$var_cpu"
-  RAM_SIZE="$var_ram"
-  BRG="vmbr0"
-  NET="dhcp"
-  GATE=""
-  APT_CACHER=""
-  APT_CACHER_IP=""
-  DISABLEIP6="no"
-  MTU=""
-  SD=""
-  NS=""
-  MAC=""
-  VLAN=""
-  SSH="no"
-  VERB="no"
-  echo_default
-}
+echo "?? Установка Zabbix внутри контейнера..."
+pct exec $CT_ID -- bash -c "apt update && apt install -y curl gnupg lsb-release"
 
-function update_script() {
-  header_info
-  if [[ ! -f /etc/zabbix/zabbix_server.conf ]]; then 
-    msg_error "No ${APP} Installation Found!"; exit 
-  fi
-  msg_info "Updating $APP LXC"
-  apt-get update &>/dev/null
-  apt-get -y upgrade &>/dev/null
-  systemctl restart zabbix-server
-  msg_ok "Updated $APP LXC"
-  exit
-}
+pct exec $CT_ID -- bash -c "
+wget https://repo.zabbix.com/zabbix/7.0/debian/pool/main/z/zabbix-release/zabbix-release_7.0-1%2Bdebian12_all.deb -O /tmp/zabbix-release.deb &&
+dpkg -i /tmp/zabbix-release.deb &&
+apt update &&
+apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent mariadb-server"
 
-# ??? Убрана проверка на минимальную версию Proxmox
+echo "?? Настройка MariaDB и Zabbix БД..."
+pct exec $CT_ID -- bash -c "
+mysql -e \"
+create database zabbix character set utf8mb4 collate utf8mb4_bin;
+create user zabbix@localhost identified by 'zabbixpass';
+grant all privileges on zabbix.* to zabbix@localhost;
+flush privileges;
+\" &&
+zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql -uzabbix -pzabbixpass zabbix"
 
-start
-build_container
-description
+echo "??? Конфигурация zabbix_server.conf"
+pct exec $CT_ID -- sed -i 's/# DBPassword=/DBPassword=zabbixpass/' /etc/zabbix/zabbix_server.conf
 
-msg_ok "Completed Successfully!\n"
-echo -e "${APP} should be reachable by going to the following URL.
-         ${BL}http://${IP}/zabbix${CL} \n"
+echo "? Запуск Zabbix-сервисов..."
+pct exec $CT_ID -- systemctl restart mariadb
+pct exec $CT_ID -- systemctl enable zabbix-server zabbix-agent apache2
+pct exec $CT_ID -- systemctl restart zabbix-server zabbix-agent apache2
+
+echo ""
+echo "?? Готово! Zabbix доступен по адресу:"
+echo "?? http://$CT_IP/zabbix"
+echo "Логин: Admin"
+echo "Пароль: zabbix"
